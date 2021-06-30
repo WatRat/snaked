8d74b999fdf2e3f3bb67501fabf7dd8d4db36ad1
WatRat <gameboy.mine123@gmail.com>
2021-07-01 00:10:19 +0300

    I do it :D

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ StartSnakePosY = 4
 head = '*'
 tail = '0'
 GAME = True
-
+SnakeEl= []
+SnakeSize= 4
 FruitPosX = random.randint(2, width - 2)
 FruitPosY = random.randint(2, height - 2)
 
@@ -38,7 +39,9 @@ class Frame:
                 elif i == height - 1:
                     print('#', end='')
                 else:
-                    if j == SnakePosX and i == SnakePosY:
+                    temps = [j,i]
+
+                    if temps in SnakeEl:
                         print('*', end='')
                     elif j == FruitPosX and i == FruitPosY:
                         print('$', end='')
@@ -51,35 +54,51 @@ class Frame:
 
 class Game:
     def start(self):
-        global StartSnakePosX, StartSnakePosY, width, height, SnakeDir
+        global StartSnakePosX
+        global StartSnakePosY
+        global width
+        global height
+        global SnakeDir
+        global SnakeEl
         x = 0
         SnakePosX = StartSnakePosX
         SnakePosY = StartSnakePosY
         frame = Frame()
         while GAME == True:
-
+            game.SnakeSizeCheck()
             if SnakeDir == 'l':
                 if SnakePosX > 2:
                     SnakePosX-=1
+
 
             if SnakeDir == 'r':
                 if SnakePosX < width -2:
                     SnakePosX+=1
 
+
             if SnakeDir == 'u':
                 if SnakePosY > 1:
                     SnakePosY-=1
+
 
             if SnakeDir == 'd':
                 if SnakePosY < height-2:
                     SnakePosY+=1
 
+
             frame.print_frame(width, height,SnakePosX, SnakePosY)
+            SnakeEl.append([SnakePosX, SnakePosY])
+            print(SnakeEl, end='\n')
             time.sleep(.2)
     def fruit(self):
         global FruitPosX, FruitPosY
         FruitPosX = random.randint(2, width - 2)
         FruitPosY = random.randint(2, height - 2)
+    def SnakeSizeCheck(self):
+        global SnakeEl
+        global SnakeSize
+        if len(SnakeEl) >= SnakeSize:
+            temp = SnakeEl.pop(0)
 
     def Up(self):
         global SnakeDir
